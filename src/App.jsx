3715ddd4493ab359e38d941d4bebb728064539e8
SimoneChiodo <simochi.005@gmail.com>
2024-12-03 15:29:44 +0100

    fix form-input

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,33 @@ import "./App.css";
 function App() {
     const [count, setCount] = useState(0);
 
-    const [articles, setArticles] = useState("");
-    let title = "";
+    // Articles Array
+    const [articles, setArticles] = useState([]);
+    // Form Data
+    let titleForm = "";
 
+    // On Form Submit
     function handleFormSubmit(e) {
         e.preventDefault();
 
-        if (title === "" || articles.includes("title")) return;
+        // Check if input is correct
+        if (titleForm === "" || articles.includes(titleForm)) return;
 
-        const newArticles = [...articles, title];
+        // Add the new Article
+        const newArticles = [...articles];
+        newArticles.push({ title: titleForm, id: getLastId(articles) });
         setArticles(newArticles);
     }
 
-    function handleInputChange(e) {
-        title = e.target.value;
+    // Get the Last ID of an Array
+    function getLastId(obj) {
+        let id = 0;
+        {
+            obj.map((element) => {
+                element.id >= id && (id = element.id + 1);
+            });
+        }
+        return id;
     }
 
     return (
@@ -29,17 +42,21 @@ function App() {
             <main className="d-flex flex-column align-items-center mt-5">
                 {/* FORM */}
                 <form onSubmit={handleFormSubmit} className="row g-3 w-50">
+                    {/* Title Input */}
                     <div className="col-12">
                         <label htmlFor="inputTitle" className="form-label">
                             Titolo articolo:
                         </label>
+
                         <input
                             type="text"
-                            onChange={handleInputChange}
+                            onChange={(e) => (titleForm = e.target.value)}
                             className="form-control"
                             id="inputTitle"
                         />
                     </div>
+
+                    {/* Submit Button */}
                     <div className="col-12">
                         <button type="submit" className="btn btn-primary">
                             Invio
@@ -50,10 +67,12 @@ function App() {
                 {/* ARTICLES LIST */}
                 <ul className="mt-5">
                     {articles?.length ? (
+                        // Print the Array
                         articles.map((article) => (
-                            <li key={article}>{article}</li>
+                            <li key={article.id}>{article.title}</li>
                         ))
                     ) : (
+                        // Print Empty Array Message
                         <h2>Ancora nessun post...</h2>
                     )}
                 </ul>
