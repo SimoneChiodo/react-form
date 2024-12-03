ee296bea5a4e8f5cebd271391dbafc9d5e49013d
SimoneChiodo <simochi.005@gmail.com>
2024-12-03 14:26:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+// Bootstrap CSS
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// Custom CSS
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <></>;
+}
+
+export default App;
